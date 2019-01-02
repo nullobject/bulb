45355ad458d879cd2eb1818ce703186bd919ae94
Josh Bassett <josh.bassett@gmail.com>
2019-01-02 16:08:18 +1100

    Clean up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -12,21 +12,17 @@ const plugins = [
 ]
 
 export default [
-  // UMD and ES versions.
   {
     input: 'src/index.js',
     output: [
-      { file: pkg.main, format: 'umd', name: 'bulb' },
+      { file: pkg.main, format: 'cjs', name: 'bulb' },
       { file: pkg.module, format: 'es' }
     ],
     plugins
-  },
-
-  // Browser minified version.
-  {
+  }, {
     input: 'src/index.js',
     output: [
-      { file: pkg.unpkg, format: 'umd', name: 'bulb' }
+      { file: pkg.unpkg, format: 'iife', name: 'bulb' }
     ],
     plugins: plugins.concat([uglify()])
   }
