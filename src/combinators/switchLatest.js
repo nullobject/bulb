bf9393a7e69ea305a1233617eb2eaccbbdae383a
Josh Bassett <josh.bassett@gmail.com>
2019-01-07 19:59:59 +1100

    Fix issue with completed signals in switchLatest

--- src/combinators/switchLatest.js
+++ src/combinators/switchLatest.js
@@ -1,27 +1,27 @@
 import Signal from '../Signal'
 
 /**
- * A higher-order signal function (operates on a signal that emits other
- * signals) that emits events from the most recent signal value.
+ * A higher-order signal (a signal that emits other signals) that emits events
+ * from the most recent signal value.
  *
- * @param {Signal} s A signal.
+ * @param {Signal} s A signal that emits other signals.
  * @returns {Signal} A new signal.
  */
 export default function switchLatest (s) {
   return new Signal(emit => {
-    let subscription2
+    let childSubscription
 
     const value = a => {
       if (!(a instanceof Signal)) { throw new Error('Signal value must be a signal') }
-      if (subscription2) { subscription2.unsubscribe() }
-      subscription2 = a.subscribe(emit)
+      if (childSubscription) { childSubscription.unsubscribe() }
+      childSubscription = a.subscribe({ value: emit.value, error: emit.error })
     }
 
-    const subscription1 = s.subscribe({ ...emit, value })
+    const parentSubscription = s.subscribe({ ...emit, value })
 
     return () => {
-      subscription1.unsubscribe()
-      if (subscription2) { subscription2.unsubscribe() }
+      parentSubscription.unsubscribe()
+      if (childSubscription) { childSubscription.unsubscribe() }
     }
   })
 }
